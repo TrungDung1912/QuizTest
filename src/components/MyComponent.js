536c6cfbb36c88d08920b25d2784ace3ad97fe79
Le Trung Dung <ekkophantoms123@gmail.com>
2023-04-17 20:27:17 +0700

    First setState

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -3,21 +3,37 @@
 
 // . Present folder
 // .. Parent folder
-// 'this' is represent for class
+// 'this' is represent for class they use
 import React from "react";
 
 class MyComponent extends React.Component {
     state = {
-        name: "Eric",
+        name: "BumBeo",
         address: "Hanoi",
         age: 20
+    }
+
+    handleClick = (event) => {
+        // console.log("Click me")
+        // console.log(e)
+        this.setState({
+            name: "BumLinhCute",
+            age: Math.floor(Math.random() * 100) + 1
+        })
+    }
+
+    handleOnMouseOver = (e) => {
+        // console.log("Mouse Over")
+        // console.log(e)
     }
 
     //JSX
     render() {
         return (
             <div>
-                My name is {this.state.name} and I'm from {this.state.address}
+                My name is {this.state.name} and I'm {this.state.age}
+                <button onMouseOver={this.handleOnMouseOver}>Hover me!!</button>
+                <button onClick={(event) => { this.handleClick(event) }}>Click me!!</button>
             </div>
         )
     }

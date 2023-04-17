96bab94f5877f5ef44b1931a7a89d4f844847171
Le Trung Dung <ekkophantoms123@gmail.com>
2023-04-17 19:58:47 +0700

    First State

--- src/App.js
+++ src/App.js
@@ -3,27 +3,36 @@ import './App.css';
 import { useDispatch, useSelector } from 'react-redux';
 import { increaseCounter, decreaseCounter } from './redux/action/counterAction';
 import MyComponent from './components/MyComponent';
-const App = () => {
-  const count = useSelector(state => state.counter.count);
-  const dispatch = useDispatch();
+import React from 'react';
 
-  return (
-    <div>Hello world with Trung Dung
-      <MyComponent></MyComponent>
-    </div>
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Hello world with Trung Dung
-    //     </p>
-    //     <div>Count = {count}</div>
-    //     <button onClick={() => dispatch(increaseCounter())}>Increase</button>
-    //     <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
-    //   </header>
-    // </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div>Hello world with Trung Dung
+        <MyComponent></MyComponent>
+      </div>
+    )
+  }
 }
+
+// const App = () => {
+//   const count = useSelector(state => state.counter.count);
+//   const dispatch = useDispatch();
+
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Hello world with Trung Dung
+//         </p>
+//         <div>Count = {count}</div>
+//         <button onClick={() => dispatch(increaseCounter())}>Increase</button>
+//         <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
 //Hot reloading: Cập nhật trang không cần refresh
--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -3,14 +3,21 @@
 
 // . Present folder
 // .. Parent folder
+// 'this' is represent for class
 import React from "react";
 
 class MyComponent extends React.Component {
+    state = {
+        name: "Eric",
+        address: "Hanoi",
+        age: 20
+    }
+
     //JSX
     render() {
         return (
-            <div>My first component
-                {Math.random()}
+            <div>
+                My name is {this.state.name} and I'm from {this.state.address}
             </div>
         )
     }

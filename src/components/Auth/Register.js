5909af2baf6361b9fbc711e4554698bd757f720c
Le Trung Dung <ekkophantoms123@gmail.com>
2023-04-25 20:29:47 +0700

    Register with APIs

--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -1,14 +1,16 @@
 import { useState } from 'react'
-import './Login.scss'
-import { useNavigate } from 'react-router-dom'
-import { postLogin } from '../../services/apiService.js'
+import './Register.scss'
 import { toast } from 'react-toastify'
+import { postRegister } from '../../services/apiService.js'
+import { useNavigate } from 'react-router-dom'
 import { VscEye, VscEyeClosed } from 'react-icons/vsc'
 
-const Login = (props) => {
+const Register = () => {
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
+    const [username, setUserName] = useState("")
     const [isShowPassword, setIsShowPassword] = useState(false)
+
     const navigate = useNavigate()
 
     const validateEmail = (email) => {
@@ -26,7 +28,7 @@ const Login = (props) => {
             );
     }
 
-    const handleLogin = async () => {
+    const handleRegister = async () => {
         //validate
         const isValidEmail = validateEmail(email);
         const isValidPassword = validatePassword(password);
@@ -39,36 +41,40 @@ const Login = (props) => {
             return;
         }
         //submit APIs
-        let data = await postLogin(email, password)
+        let data = await postRegister(email, password, username)
         if (data && data.EC === 0) {
             toast.success(data.EM)
-            navigate('/')
+            navigate('/login')
         }
         if (data && data.EC !== 0) {
             toast.error(data.EM)
         }
     }
 
-    const handleNavigateRegister = () => {
-        navigate('/register')
+    const handleNavigateLogin = () => {
+        navigate('/login')
     }
 
     return (
-        <div className="login-container">
+        <div className="register-container">
             <div className='header'>
-                <span>Don't have an account yet?</span>
-                <button onClick={() => { handleNavigateRegister() }}>Sign up</button>
+                <span>Do have an account yet?</span>
+                <button onClick={() => { handleNavigateLogin() }}>Log in</button>
             </div>
             <div className='title col-4 mx-auto'>
                 DungBumBeo
             </div>
             <div className='welcome col-4 mx-auto'>
-                Hello, who’s this?
+                Let's go, sir?
             </div>
             <div className='content-form col-4 mx-auto'>
                 <div className='form-group'>
-                    <label>Email</label>
+                    <label>Email(*)</label>
                     <input onChange={(event) => setEmail(event.target.value)} value={email} className='form-control' type='email' />
+                </div>
+                <div className='form-group'>
+                    <label>Username</label>
+                    <input onChange={(event) => setUserName(event.target.value)} value={username} className='form-control' type='text' />
                 </div>
                 <div className='form-group password-group'>
                     <label>Password(*)</label>
@@ -84,9 +90,8 @@ const Login = (props) => {
                         </span>
                     }
                 </div>
-                <span className='forgot-password'>Forgot Password?</span>
                 <div>
-                    <button onClick={() => { handleLogin() }} className='btn-submit'>Login</button>
+                    <button onClick={() => { handleRegister() }} className='btn-submit'>Register</button>
                 </div>
                 <div className='text-center'>
                     <span className='back' onClick={() => { navigate('/') }}> &#60; &#60; Come back home &#62; &#62;</span>
@@ -96,4 +101,4 @@ const Login = (props) => {
     )
 }
 
-export default Login
+export default Register
--- src/services/apiService.js
+++ src/services/apiService.js
@@ -33,7 +33,10 @@ const getUserWithPaginate = (page, limit) => {
 
 const postLogin = (email, password) => {
     return axios.post(`api/v1/login`, { email, password })
-
 }
 
-export { postCreateNewUser, getAllUsers, putUpdateUser, deleteUser, getUserWithPaginate, postLogin }
+const postRegister = (email, password, username) => {
+    return axios.post(`api/v1/register`, { email, password, username })
+}
+
+export { postCreateNewUser, getAllUsers, putUpdateUser, deleteUser, getUserWithPaginate, postLogin, postRegister }
